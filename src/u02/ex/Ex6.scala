a69b656b17ca1965ac8d7260faaba98eae26b366
Federico Mazzini <federico.mazzini3@studio.unibo.it>
2021-03-09 13:18:38 +0100

    fibonacci tail recursion

--- src/u02/ex/Ex6.scala
+++ src/u02/ex/Ex6.scala
@@ -2,6 +2,7 @@ package u02.ex
 
 /*
     Write a function to get the n-th Fibonacci number
+    without and with tail recursion
  */
 object Ex6 {
 
@@ -9,5 +10,15 @@ object Ex6 {
     case 0 => 0
     case 1 => 1
     case n => fibonacci(n-1) + fibonacci(n-2)
+  }
+
+  //fibonacci tail recursive
+  def fibonacciTail(n: Int): Int = {
+    @annotation.tailrec
+    def _fib(n: Int, a: Int, b: Int): Int = n match {
+      case 0 => b
+      case _ => _fib(n-1, a + b, a)
+    }
+    _fib(n, 1, 0)
   }
 }
--- test/u02/ex/Ex6Test.scala
+++ test/u02/ex/Ex6Test.scala
@@ -1,11 +1,16 @@
 package u02.ex
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import u02.ex.Ex6._
 
 class Ex6Test {
   //es 6
   @Test def testFibonacci(): Unit ={
-    println(196418, fibonacci(27))
+    assertEquals(196418, fibonacci(27))
+  }
+
+  @Test def testFibonacciTailRecursion(): Unit ={
+    assertEquals(196418, fibonacciTail(27))
   }
 }
